055e7fba8e6b701292d10748139cc932d9f046e5
fr1t2 <fr1t2@protonmail.com>
2018-02-19 17:08:58 -0700

    Initial commin. Site running at qrlmining.com

---
+++ config.js
@@ -0,0 +1,19 @@
+var api = "http://66.175.217.203:11982";
+
+var coinUnits = 1000000000000;
+
+var poolHost = "http://strat.qrlmining.com";
+
+var irc = "irc.freenode.net/#monero-pools";
+
+var email = "fr1t2@qrlmining.com";
+
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+var easyminerDownload = "http://monero.qrlmining.com/mine/cryptonote-easy-miner.zip";
+
+var blockchainExplorer = "http://chainradar.com/xmr/block/";
+
+var transactionExplorer = "http://chainradar.com/xmr/transaction/";
+
+
