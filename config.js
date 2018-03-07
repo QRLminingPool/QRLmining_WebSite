0e2aa932c9565c71ff9fda496e42d349f1860105
oisn1 <oisn1@fr1t2pool.com>
2018-03-07 13:36:23 -0500

    Name update and test Commit

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var poolHost = "http://strat.qrlmining.com";
 
 var irc = "irc.freenode.net/#monero-pools";
 
-var email = "fr1t2@qrlmining.com";
+var email = "qrlmining@qrlmining.com";
 
 var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
 
